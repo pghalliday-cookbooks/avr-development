835cbb804a8411836b009fcdefdff57aaf057faa
Peter Halliday <pghalliday@gmail.com>
2016-07-05 22:27:16 +0200

    refactor dotfiles into it's own cookbook for sharing

--- cookbooks/avr_development/attributes/default.rb
+++ cookbooks/avr_development/attributes/default.rb
@@ -1,5 +1,3 @@
 default['avr_development']['user'] = nil
 default['avr_development']['group'] = nil
 default['avr_development']['home'] = nil
-default['avr_development']['ssh']['id_rsa'] = '/root/.ssh/id_rsa'
-default['avr_development']['ssh']['authorized_keys'] = '/root/.ssh/authorized_keys'
--- cookbooks/avr_development/recipes/default.rb
+++ cookbooks/avr_development/recipes/default.rb
@@ -1,2 +1,6 @@
-include_recipe 'avr_development::dotfiles'
+node.default['dotfiles']['user'] = node['avr_development']['user']
+node.default['dotfiles']['group'] = node['avr_development']['group']
+node.default['dotfiles']['home'] = node['avr_development']['home']
+
+include_recipe 'dotfiles::default'
 include_recipe 'avr_development::avr'
